0180641b1ca239d3a3fbb70e6aa0eba3ef6ec095
pascalmlaurent@gmail.com <plaurent@pascal.jeremieonline.com>
2022-05-06 09:03:09 -0400

    using integer test case

---
+++ 2-adder/adder_test.go
@@ -0,0 +1,20 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(3, 2)
+	expected := 3
+
+	if sum != expected {
+		go t.Errorf("Expected %d but got %d", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+}
---
+++ 3-iterations'.go
@@ -0,0 +1,1 @@
+package golang_with_test
