e3372ee5a695e873d86a048f59c6c1c2f1677c72
Plezi Devops <plezidevops@gmail.com>
2022-05-09 13:39:39 -0400

    rafactor using an interface

--- 5-structs-methods-interfaces/perimeter_test.go
+++ 5-structs-methods-interfaces/perimeter_test.go
@@ -13,23 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f but want %.2f", got, want)
 		}
+	}
+
+	t.Run("Rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("Circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
