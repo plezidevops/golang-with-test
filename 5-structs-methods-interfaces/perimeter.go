5ad06d75dc34b938d481fded00d03515eecb724a
Plezi Devops <plezidevops@gmail.com>
2022-05-09 12:24:08 -0400

    calculate area of a rectangle and circle

--- 5-structs-methods-interfaces/perimeter.go
+++ 5-structs-methods-interfaces/perimeter.go
@@ -1,14 +1,24 @@
 package perimeter
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
 }
