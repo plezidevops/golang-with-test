0b60764182e7a65b0df132bc1b7c289d74553828
Plezi Devops <plezidevops@gmail.com>
2022-05-09 10:33:03 -0400

    refactor using structs

---
+++ 5-structs-methods-interfaces/perimeter.go
@@ -0,0 +1,14 @@
+package perimeter
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
+}
